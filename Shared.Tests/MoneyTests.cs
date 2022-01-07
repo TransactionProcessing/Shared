c50a980aee6b0ff7475ef5efcae942725680a525
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2022-01-07 20:50:14 +0000

    Added money value objects

---
+++ Shared.Tests/MoneyTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Tests
+{
+    using System.Diagnostics.PerformanceData;
+    using Shouldly;
+    using ValueObjects;
+    using Xunit;
+
+    public class MoneyTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1.00)]
+        [InlineData(1.59)]
+        [InlineData(100.00)]
+        [InlineData(-1.59)]
+        public void Money_CanBeCreated_IsCreated(Decimal moneyValue)
+        {
+            Money money = Money.Create(moneyValue);
+
+            money.ShouldNotBeNull();
+            money.Value.ShouldBe(moneyValue);
+        }
+
+        [Theory]
+        [InlineData(1,100,101)]
+        [InlineData(0, 100, 100)]
+        [InlineData(-1, 100, 99)]
+        [InlineData(1.01, 100.01, 101.02)]
+        [InlineData(0.01, 100.01, 100.02)]
+        [InlineData(-1.01, 100.01, 99)]
+        public void Money_AddOperator_ValueAdded(Decimal initialValue, Decimal valueToAdd, Decimal expectedResult)
+        {
+            Money money1 = Money.Create(initialValue);
+            Money money2 = Money.Create(valueToAdd);
+
+            var result = money1 + money2;
+            result.Value.ShouldBe(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(1, 100, -99)]
+        [InlineData(0, 100, -100)]
+        [InlineData(-1, 100, -101)]
+        [InlineData(1.01, 100.01, -99)]
+        [InlineData(0.01, 100.01, -100)]
+        [InlineData(-1.01, 100.01, -101.02)]
+        public void Money_SubtractOperator_ValueAdded(Decimal initialValue, Decimal valueToSubtract, Decimal expectedResult)
+        {
+            Money money1 = Money.Create(initialValue);
+            Money money2 = Money.Create(valueToSubtract);
+
+            var result = money1 - money2;
+            result.Value.ShouldBe(expectedResult);
+        }
+    }
+
+    public class PositiveMoneyTests
+    {
+        [Theory]
+        [InlineData(1.00)]
+        [InlineData(1.59)]
+        [InlineData(100.00)]
+        public void PositiveMoney_CanBeCreated_IsCreated(Decimal moneyValue)
+        {
+            PositiveMoney money = PositiveMoney.Create(Money.Create(moneyValue));
+
+            money.ShouldNotBeNull();
+            money.Value.ShouldBe(moneyValue);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void PositiveMoney_NonPositiveAmountRejected_ErrorThrown(Decimal moneyValue)
+        {
+            Should.Throw<ArgumentOutOfRangeException>(() =>
+                                                      {
+                                                          PositiveMoney money = PositiveMoney.Create(Money.Create(moneyValue));
+                                                      });
+        }
+    }
+}
---
+++ Shared/ValueObjects/Money.cs
@@ -0,0 +1,78 @@
+﻿namespace Shared.ValueObjects
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="System.IEquatable&lt;Shared.ValueObjects.Money&gt;" />
+    public record Money
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Money"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        private Money(Decimal value)
+        {
+            this.Value = value;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public Decimal Value { get; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Creates the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static Money Create(Decimal value)
+        {
+            return new Money(value);
+        }
+
+        /// <summary>
+        /// Implements the operator +.
+        /// </summary>
+        /// <param name="leftHand">The left hand.</param>
+        /// <param name="rightHand">The right hand.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Money operator +(Money leftHand,
+                                       Money rightHand)
+        {
+            return new Money(leftHand.Value + rightHand.Value);
+        }
+
+        /// <summary>
+        /// Implements the operator -.
+        /// </summary>
+        /// <param name="leftHand">The left hand.</param>
+        /// <param name="rightHand">The right hand.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Money operator -(Money leftHand,
+                                       Money rightHand)
+        {
+            return new Money(leftHand.Value - rightHand.Value);
+        }
+
+        #endregion
+    }
+}
