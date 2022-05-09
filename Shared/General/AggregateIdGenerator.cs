f22736f9868ad37f1f2d9e01a2e8d68d887ae1f0
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2022-05-09 21:42:24 +0100

    Add GuidCalculator to Shared

---
+++ Shared/General/AggregateIdGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.General
+{
+    using System;
+    using Extensions;
+
+    public static class AggregateIdGenerator
+    {
+        public static Guid CalculateSettlementAggregateId(DateTime settlementDate,
+                                                          Guid estateId)
+        {
+            Guid aggregateId = GuidCalculator.Combine(estateId, settlementDate.ToGuid());
+            return aggregateId;
+        }
+    }
+}
---
+++ Shared/General/GuidCalculator.cs
@@ -0,0 +1,56 @@
+﻿namespace Shared.General
+{
+    using System;
+
+    public static class GuidCalculator
+    {
+        #region Methods
+
+        public static Guid Combine(Guid firstGuid,
+                                   Guid secondGuid,
+                                   Byte offset) {
+            Byte[] firstAsBytes = firstGuid.ToByteArray();
+            Byte[] secondAsBytes = secondGuid.ToByteArray();
+
+            Byte[] newBytes = new Byte[16];
+
+            for (Int32 i = 0; i < 16; i++) {
+                // Add and truncate any overflow
+                newBytes[i] = (Byte)(firstAsBytes[i] + secondAsBytes[i] + offset);
+            }
+
+            return new Guid(newBytes);
+        }
+
+        public static Guid Combine(Guid firstGuid,
+                                   Guid secondGuid) {
+            return GuidCalculator.Combine(firstGuid, secondGuid, 0);
+        }
+
+        public static Guid Combine(Guid firstGuid,
+                                   Guid secondGuid,
+                                   Guid thirdGuid,
+                                   Byte offset) {
+            Byte[] firstAsBytes = firstGuid.ToByteArray();
+            Byte[] secondAsBytes = secondGuid.ToByteArray();
+            Byte[] thirdAsBytes = thirdGuid.ToByteArray();
+
+            Byte[] newBytes = new Byte[16];
+
+            for (Int32 i = 0; i < 16; i++) {
+                // Add and truncate any overflow
+                newBytes[i] = (Byte)(firstAsBytes[i] + secondAsBytes[i] + thirdAsBytes[i] + offset);
+            }
+
+            return new Guid(newBytes);
+        }
+
+        public static Guid Combine(Guid firstGuid,
+                                   Guid secondGuid,
+                                   Guid thirdGuid) {
+            return GuidCalculator.Combine(firstGuid, secondGuid, thirdGuid, 0);
+        }
+
+        #endregion
+    }
+}
