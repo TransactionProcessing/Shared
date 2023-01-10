1574f870e7e610c92ad590b728378700d8e4a414
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2023-01-10 20:49:05 +0000

    Ready for PR

--- Shared.IntegrationTesting/DockerHelper.cs
+++ Shared.IntegrationTesting/DockerHelper.cs
@@ -78,7 +78,7 @@ public class DockerHelper : BaseDockerHelper
             _ => true
         };
         this.SetHostTraceFolder(scenarioName);
-        
+
         Logging.Enabled();
 
         this.TestId = Guid.NewGuid();
@@ -89,71 +89,57 @@ public class DockerHelper : BaseDockerHelper
 
         this.ClientDetails = ("serviceClient", "Secret1");
 
-        //try {
+        INetworkService testNetwork = this.SetupTestNetwork();
+        this.TestNetworks.Add(testNetwork);
 
+        var networks = new List<INetworkService>();
+        networks.Add(this.SqlServerNetwork);
+        networks.Add(testNetwork);
 
-            INetworkService testNetwork = this.SetupTestNetwork();
-            this.TestNetworks.Add(testNetwork);
+        await this.SetupEventStoreContainer(testNetwork, isSecure:this.IsSecureEventStore);
 
-            var networks = new List<INetworkService>();
-            networks.Add(this.SqlServerNetwork);
-            networks.Add(testNetwork);
-            
-            await this.SetupEventStoreContainer(testNetwork, isSecure:this.IsSecureEventStore);
+        await this.SetupMessagingServiceContainer(networks);
 
-            await this.SetupMessagingServiceContainer(networks);
+        await this.SetupSecurityServiceContainer(networks);
 
-            await this.SetupSecurityServiceContainer(networks);
+        await this.SetupCallbackHandlerContainer(networks);
 
-            await this.SetupCallbackHandlerContainer(networks);
+        await this.SetupTestHostContainer(networks);
 
-            await this.SetupTestHostContainer(networks);
+        await this.SetupEstateManagementContainer(networks);
 
-            await this.SetupEstateManagementContainer(networks);
+        await this.SetupTransactionProcessorContainer(networks);
 
-            await this.SetupTransactionProcessorContainer(networks);
+        await this.SetupFileProcessorContainer(networks);
 
-            await this.SetupFileProcessorContainer(networks);
+        await this.SetupVoucherManagementAclContainer(networks);
 
-            await this.SetupVoucherManagementAclContainer(networks);
+        await this.SetupTransactionProcessorAclContainer(networks);
 
-            await this.SetupTransactionProcessorAclContainer(networks);
+        await this.LoadEventStoreProjections();
 
-            await this.LoadEventStoreProjections();
-
-            await this.CreateGenericSubscriptions();
-        //}
-        //catch(Exception e) {
-        //    this.Trace(e.Message);
-        //}
+        await this.CreateGenericSubscriptions();
     }
 
-    public override async Task StopContainersForScenarioRun()
-    {
-        //try {
+    public override async Task StopContainersForScenarioRun() {
+        if (this.Containers.Any()) {
+            this.Containers.Reverse();
 
-            if (this.Containers.Any()) {
-                this.Containers.Reverse();
-
-                foreach (IContainerService containerService in this.Containers) {
-                    this.Trace($"Stopping container [{containerService.Name}]");
-                    containerService.StopOnDispose = true;
-                    containerService.RemoveOnDispose = true;
-                    containerService.Dispose();
-                    this.Trace($"Container [{containerService.Name}] stopped");
-                }
+            foreach (IContainerService containerService in this.Containers) {
+                this.Trace($"Stopping container [{containerService.Name}]");
+                containerService.StopOnDispose = true;
+                containerService.RemoveOnDispose = true;
+                containerService.Dispose();
+                this.Trace($"Container [{containerService.Name}] stopped");
             }
+        }
 
-            if (this.TestNetworks.Any()) {
-                foreach (INetworkService networkService in this.TestNetworks) {
-                    networkService.Stop();
-                    networkService.Remove(true);
-                }
+        if (this.TestNetworks.Any()) {
+            foreach (INetworkService networkService in this.TestNetworks) {
+                networkService.Stop();
+                networkService.Remove(true);
             }
-        //}
-        //catch(Exception e) {
-        //    this.Trace(e.Message);
-        //}
+        }
     }
 
     public virtual async Task CreateGenericSubscriptions() {
