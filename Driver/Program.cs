a08b1c2d68a0c9c86e786973eeeacc228d1d5956
StuartFerguson <stuart_ferguson1development@outlook.com>
2025-09-24 11:16:24 +0100

    Update Program.cs

--- Driver/Program.cs
+++ Driver/Program.cs
@@ -40,6 +40,9 @@ internal class Program
         return settings;
     }
 
+    public static void TestFunction(String param1,String param2,String param3,String param4,String param5,String param6,String param7,String param8,String param9,String param10,String param11){
+    }
+    
     private static async Task Main(String[] args) {
         Logger.Initialise(NullLogger.Instance);
         await Program.SubscriptionsTest();
